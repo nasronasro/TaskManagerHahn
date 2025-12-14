0249a152ea739e8029408050b4789b619bc38a5b
nasronasro <ahfir49481@gmail.com>
2025-12-14 18:00:55 +0100

    fix: update the correct Task Model

--- Models/Task.cs
+++ Models/Task.cs
@@ -6,6 +6,7 @@
         public string Title { get; set; }
         public string? Description { get; set; }
         public DateTime DueDate { get; set; }
+        public bool Completed { get; set; }
         public int ProjectId { get; set; }
         public Project? Project { get; set; }
     }
