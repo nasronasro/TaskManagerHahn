bda7657b5efbe5fc6a0ce73064fd7abe00715299
nasronasro <ahfir49481@gmail.com>
2025-12-14 19:17:06 +0100

    feat: Implement TaskDelete TaskController

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -15,6 +15,7 @@ namespace ProjectTasksManager.Controllers
         {
             this.taskService = taskService;
         }
+        [HttpPost]
          public async Task<IActionResult> Create(TaskCreateDto taskDto)
          {
             if (!ModelState.IsValid) return BadRequest();
@@ -31,7 +32,32 @@ namespace ProjectTasksManager.Controllers
             {
                 return NotFound(new { ex.Message });
             }
-             
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new { Error = "An unexpected server error occurred during Creating a Task." });
+            }
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                string? userEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+                if (userEmail == null)
+                    return Unauthorized();
+                await taskService.DeleteTask(id);
+                return NoContent();
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new { ex.Message });
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new { Error = "An unexpected server error occurred during Deleting a Task." });
+            }
         }
     }
 }
